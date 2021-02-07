7c1c35bec212c668d0d01ff9729e9aaba70faebc
PabloSato <pablofersato@gmail.com>
2021-02-07 14:27:49 +0100

    add metodo sumaDosEnteros

--- Actividad2/src/calculadora/Suma.java
+++ Actividad2/src/calculadora/Suma.java
@@ -2,8 +2,12 @@ package calculadora;
 
 public class Suma {
 
-	
 	public double sumaDosReales(double num1, double num2) {
+
+		return num1 + num2;
+	}
+
+	public int sumaDosEnteros(int num1, int num2) {
 
 		return num1 + num2;
 	}
