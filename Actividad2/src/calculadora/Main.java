b2fbd2c8eeef867bff7b05158dd187ef53d0b548
Sergio Gomez <sergio.gmzdl@gmail.com>
2021-02-07 18:06:38 +0100

    Cociente

--- Actividad2/src/calculadora/Main.java
+++ Actividad2/src/calculadora/Main.java
@@ -4,8 +4,21 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//Crucemos los dedos para que vaya bien
+		
+		Cociente coc = new Cociente();
+		coc.divisionDecimales(10.2,0.50);
+		System.out.println(coc.consultarDouble());
+		System.out.println("***************************");
+		Cociente coc2 = new cociente();
+		coc2.division(6, 3);
+		System.out.println(coc2.consultar());
+		System.out.println("***************************");
+		coc2.raiz(8);
+		System.out.println(coc2.consultarRaiz());
+		System.out.println("***************************");
+		coc2.inverso(2);
+		System.out.println(coc2.consultarInverso());
 
 	}
-
+	
 }
