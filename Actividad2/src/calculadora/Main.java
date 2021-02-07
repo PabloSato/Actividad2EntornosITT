2e2fc0905a2c03e76f5bc2f18e466355a3c3cefb
PabloSato <pablofersato@gmail.com>
2021-02-07 18:32:39 +0100

    add metodos Suma al Main

--- Actividad2/src/calculadora/Main.java
+++ Actividad2/src/calculadora/Main.java
@@ -9,7 +9,7 @@ public class Main {
 		coc.divisionDecimales(10.2,0.50);
 		System.out.println(coc.consultarDouble());
 		System.out.println("***************************");
-		Cociente coc2 = new cociente();
+		Cociente coc2 = new Cociente();
 		coc2.division(6, 3);
 		System.out.println(coc2.consultar());
 		System.out.println("***************************");
@@ -18,6 +18,14 @@ public class Main {
 		System.out.println("***************************");
 		coc2.inverso(2);
 		System.out.println(coc2.consultarInverso());
+		
+		//METODOS DE LA CLASE SUMA
+		Suma suma = new Suma();
+		System.out.println(suma.sumaDosReales(4, 5));
+		System.out.println(suma.sumaDosEnteros(7, 6));
+		System.out.println(suma.sumaTresReales(10, 20, 30));
+		suma.valorAcumulado(10);
+		suma.valorAcumulado(20);
 
 	}
 	
