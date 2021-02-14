2c3a79cc4a05dd6fcd2892e6f979dbecdaf0298d
Ana Pardo Navarro <jgd237@msn.com>
2021-02-14 20:03:18 +0100

    Añadidos ultimos detalles de la clase cocienteJorge. Finalizado

--- Actividad2/src/calculadora/cocienteJorge.java
+++ Actividad2/src/calculadora/cocienteJorge.java
@@ -2,7 +2,20 @@ package calculadora;
 
 public class cocienteJorge {
 
-
+	/**
+	 * La clase cocienteJorge puede realizar las siguientes cuatro operaciones: 
+	 *-Division con numero entero
+	 *-Division con decimales 
+	 *-Inverso de un numero real 
+	 *-Raiz cuadrada.
+	 *Contiene cuatro metodos que realizan las distintas operaciones algoritmicas y otros  
+	 * tres metodos que devuelven unicamente el valor de las operaciones realizadas (numero entero, 
+	 * o real). 
+	 * 
+	 * @author Jorge García
+	 * @since  14/02/2021
+	 *
+	 */
 
 	
 
@@ -11,6 +24,14 @@ public class cocienteJorge {
 	private double resultadoRaiz2;
 	private double resultadoInverso2;
 	
+	/** 
+	 * Este metodo le atribuimos parametros a las variables para que esten inicializadas, dando valor
+	 * 0 a los de tipo entero y 0.0 a los de tipo Real
+	 * 
+	 * @author Jorge García
+	 * @since 14/02/2021
+	 * 	
+	 */
 
 	public cocienteJorge() {
 
@@ -21,6 +42,23 @@ public class cocienteJorge {
 		
 	}
 
+	/** 
+	 * Este metodo realiza la <b>division<b> de dos <b>numeros enteros<b>.
+	 * 
+	 * @param operando1 es el primer valor entero que se introduce para la realizacion de la division. 
+	 * @param operando2 es el segundo valor entero introducido a dividir. 
+	 * @param resultado es el total de la operacion entre el operando1 y operando2, que devuelve un 
+	 * 		  numero entero.
+	 * @return la division entre ambos numeros.
+	 * 
+	 * @exception 
+	 * Se ejecutara el metodo siempre y cuando el resto de la division sea 0 y en caso
+	 * de introducir valor mayor, o igual a 0 (a los operandos). Para ello se emplea los condicionales if.
+	 * 
+	 * @author Sergio y Jorge
+	 * @since 24/01/2021
+	 * 
+	 */
 
 		public int division2 (int operando1, int operando2) {
 
@@ -40,7 +78,22 @@ public class cocienteJorge {
 }		
 	
 
-
+		/** 
+		 * Este metodo realiza la <b>division<b> de <b>dos numeros reales<b>.
+		 * 
+		 * @param operando1 es el primer valor real que se introduce para la realizacion de la division. 
+		 * @param operando2 es el segundo valor real que se introduce para la division. 
+		 * @param resultado es el total de la operacion algoritmica. El resultado sera un numero real.
+		 * @return la division de ambos numeros.
+		 * 
+		 * @exception
+		 * Se ejecutara el metodo en caso de que se cumpla la siguiente condicion: 
+		 * Valor mayor o igual a 0.
+		 * 
+		 * @author Sergio&Jorge
+		 * @since 27/01/2021
+		 * 
+		 */
 		
 		public double divisionDecimales2 (double operando1, double operando2) {
 
@@ -54,7 +107,19 @@ public class cocienteJorge {
 			return resultadoDouble2;		
 }
 
-
+		/** 
+		 * Este metodo realiza la <b>raiz cuadrada de un numero entero<b>.
+		 * 
+		 * @param operando es el valor entero introducido para la realizacion de la raiz cuadrada. 
+		 * @param resultadoRaiz variable que se le asigna el resultado real de la raiz cuadrada. 
+		 * @param Math.sqrt se utiliza para la realizacion de la raiz cuadrada. 
+		 * 
+		 * @return numero real resultante de la raiz de numero entero.
+		 * 
+		 * @author Sergio&Jorge
+		 * @since 27/01/2021
+		 * 
+		 */
 		
 		public double raiz2 (int operando1) {
 				
@@ -63,6 +128,22 @@ public class cocienteJorge {
 				
 		}
 
+		/** 
+		 * Este metodo realiza el <b>numero inverso de un numero real<b> introducido.
+		 * 
+		 * @param operando1 asigna un numero real a la variable, que se introduce para la realizacion del metodo inverso. 
+		 * @return resultado es el total de la operacion algoritmica, devuelve un numero real.
+		 * 
+		 * @exception
+		 * El metodo se realizara siempre y cuando el operando sea distinto a 0.
+		 * Para ello se emplea la condicion if.
+		 * 
+		 * @author Sergio&Jorge
+		 * @since 27/01/2021
+		 * 
+		 */
+		
+		
 		public double inverso2 (double operando1) {
 		
 
@@ -76,6 +157,17 @@ public class cocienteJorge {
 			
 	}
 	
+		
+		/** 
+		 * Este metodo se encarga de la consulta y devolucion del <b>resultado<b> resultante de la
+		 * <b>division de los dos numeros enteros introducidos<b>
+		 *  
+		 * @return la division con nº enteros.
+		 * 
+		 * @author Jorge
+		 * @since 14/02/2021
+		 * 
+		 */	
 
 		
 		String consultar2() {
@@ -84,6 +176,16 @@ public class cocienteJorge {
 			
 		}
 
+		/** 
+		 * Este metodo se encarga de la consulta y devolucion del <b>resultado<b> obtenido de la
+		 * <b>division de los dos numeros reales introducidos<b>
+		 *  
+		 * @return la division de los dos numeros reales
+		 * 
+		 * @author Jorge 
+		 * @since 14/02/2021
+		 * 
+		 */	
 		
 		public String consultarDouble2() {
 			
@@ -92,6 +194,15 @@ public class cocienteJorge {
 		}
 		
 
+		/** 
+		 * Este metodo se encarga de realizar la <b>raiz<b> de <b>un numero real<b>
+		 *  
+		 * @return la raiz con numero real
+		 * 
+		 * @author Jorge	
+		 * @since 14/02/2021
+		 * 
+		 */
 		
 		public String consultarRaiz2() {
 			
@@ -100,7 +211,16 @@ public class cocienteJorge {
 		}
 		
 
-				
+		/** 
+		 * Este metodo se encarga de realizar el <b>inverso<b> de <b>un numero real<b>
+		 *  
+		 * @return  del inverso con numero real
+		 * 
+		 * @author Jorge	
+		 * @since 14/02/2021
+		 * 
+		 */
+		
 		public String consultarInverso2() {
 					
 			return("El resultado del inverso de un numero entero es: " + this.resultadoInverso2);
