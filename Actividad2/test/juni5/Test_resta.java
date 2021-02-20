35675531736f47b62d67d7b973246a652468f8e2
PabloSato <pablofersato@gmail.com>
2021-02-20 21:54:08 +0100

    add Before y after

--- Actividad2/test/juni5/Test_resta.java
+++ Actividad2/test/juni5/Test_resta.java
@@ -2,10 +2,41 @@ package juni5;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class Test_resta {
 
+	/**
+	 * Pruebas Unitarias de la clase Resta
+	 * 
+	 * @author pablo Fernández Sato
+	 */
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception{
+		
+	}
+	
+	@AfterAll
+	static void tearDownAfterClass() throws Exception{
+		
+	}
+	
+	@BeforeEach
+	void setUp() throws Exception{
+		
+	}
+	
+	@AfterEach
+	void tearDown() throws Exception{
+		
+	}
+	
+	
 	@Test
 	void test() {
 		fail("Not yet implemented");
