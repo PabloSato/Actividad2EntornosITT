83c403fe22be443f9194d8bf2eee88bf980e74bf
PabloSato <pablofersato@gmail.com>
2021-02-20 21:42:13 +0100

    add Folder test y JUnit

---
+++ Actividad2/test/juni5/Test_resta.java
@@ -0,0 +1,14 @@
+package juni5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Test_resta {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
