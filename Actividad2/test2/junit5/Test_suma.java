d9dd6031cdc07da0c4139ad9b8391cc632753466
Sergio Gomez <sergio.gmzdl@gmail.com>
2021-02-21 19:05:28 +0100

    Add Junit y primeros pasos test Suma

--- Actividad2/test2/junit5/Test_suma.java
+++ Actividad2/test2/junit5/Test_suma.java
@@ -2,17 +2,58 @@ package junit5;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class Test_suma {
+import calculadora.Suma;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-<<<<<<< HEAD
+class test_Suma {
+	
+	private Suma sum1;
+
+	/**
+	 * Pruebas unitarias de la clase Suma
+	 * 
+	 * @author Sergio Gomez 
+	 * @since 21/02/2021
+	 * 
+	 */
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception{
 		
-=======
->>>>>>> master
 	}
-
+	
+	@AfterAll
+	static void tearDownAfterClass() throws Exception{
+		
+	}
+	
+	@BeforeEach
+	void setUp() throws Exception{
+		sum1 = new Suma();
+	}
+	
+	@AfterEach
+	void tearDown() throws Exception{
+		sum1 = null;
+	}
+	
+	@Test
+	void testAcumulador() {
+		sum1.toString();
+		
+		assertEquals(1, sum1.sumaDosEnteros(1,1));
+		
+		assertNotEquals(1, sum1.sumaDosEnteros(0,0));
+		
+	}
+	
 }
+	
+	
+	
+	
